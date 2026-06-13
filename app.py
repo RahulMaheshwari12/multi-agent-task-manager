b464490a53a5744eed08bfa83e8d95166b275726
= <rahulmaheshwari529@gmail.com>
2026-06-13 16:02:29 +0530

    changed host

--- app.py
+++ app.py
@@ -199,7 +199,7 @@ async def root():
 if __name__ == "__main__":
     uvicorn.run(
         "app:app",
-        host="0.0.0.0",
+        host="127.0.0.1",
         port=8000,
         reload=True
     )
